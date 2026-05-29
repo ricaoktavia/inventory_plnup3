14553ff8dea05ba6f3034245a652034ed19b12bf
iffawww <afifatunnhryu@gmail.com>
2026-05-29 10:47:44 +0700

    layout

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -3,7 +3,12 @@ import type { Actions } from './$types';
 
 export const actions: Actions = {
 	default: async ({ cookies }) => {
-		cookies.delete('session', { path: '/' });
+		cookies.delete('session', { 
+			path: '/',
+			httpOnly: true,
+			sameSite: 'lax',
+			secure: false
+		});
 		throw redirect(302, '/login');
 	}
 };
